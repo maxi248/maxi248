903c150703a046051081b67c769fa8f3200fecde
maxi248 <138704681+maxi248@users.noreply.github.com>
2024-04-12 19:03:43 +0200

    2

---
+++ Platformer/Lernzettel240412.py
@@ -0,0 +1,9 @@
+
+# TODO
+# Schau dir folgende zwei Seiten an und vollziehe nach, wie eigene Klassen für Gegner programmiert werden können
+# Man könnte auch ohne eine eigene Klasse für diese zu erstellen, dem Spiel Gegner hinzufügen, es wäre aber schlechter strukturierter Code
+# https://api.arcade.academy/en/2.6.0/examples/sprite_move_keyboard.html
+# https://api.arcade.academy/en/latest/examples/platform_tutorial/step_14.html
+# Implementiere dann eine eigene Gegner-Klasse, die mindestens die beiden Methoden __init__ und update/on_update besitzt
+# Der Gegner soll eine Bewegung ausführen
+# Wenn der Spieler den Gegner berührt, soll das Spiel beendet werden
