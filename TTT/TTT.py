d085fcc27d06efcc4c4c2e2d912cb2c084ab9ffe
maxi248 <138704681+maxi248@users.noreply.github.com>
2024-06-14 18:39:44 +0200

    1

--- TTT/TTT.py
+++ TTT/TTT.py
@@ -1,4 +1,6 @@
 import arcade
+import arcade.color
+
 
 class TTT(arcade.Window):
     def __init__(self):
@@ -6,18 +8,59 @@ class TTT(arcade.Window):
         self.arena = {(0, 0): "", (1, 0): "", (2, 0): "", (0, 1): "", (1, 1): "", (2, 1): "", (0, 2): "", (1,  2): "", (2, 2): "" }
         arcade.set_background_color(arcade.color.BLACK)
         self.spieler = "X"
+        self.reset()
+
+    def reset(self):
+        self.arena = {(0, 0): "", (1, 0): "", (2, 0): "", (0, 1): "", (1, 1): "", (2, 1): "", (0, 2): "", (1,  2): "", (2, 2): "" }
+        self.spieler = "X"
+        self.freie_felder = []
+
+    def _gewinnprüfung(self):
+
+        if self.arena[(0, 0)] == self.arena[(1, 0)] == self.arena[(2, 0)] != "":
+            return True
+        if self.arena[(0, 1)] == self.arena[(1, 1)] == self.arena[(2, 1)] != "":
+            return True
+        if self.arena[(0, 2)] == self.arena[(1, 2)] == self.arena[(2, 2)] != "":
+            return True
+        if self.arena[(0, 0)] == self.arena[(0, 1)] == self.arena[(0, 2)] != "":
+            return True
+        if self.arena[(1, 0)] == self.arena[(1, 1)] == self.arena[(1, 2)] != "":
+            return True
+        if self.arena[(2, 0)] == self.arena[(2, 1)] ==  self.arena[(2, 2)] != "":
+            return True
+        if self.arena[(0, 0)] == self.arena[(1, 1)] ==  self.arena[(2, 2)] != "":
+            return True
+        if self.arena[(0, 2)] == self.arena[(1, 1)] ==  self.arena[(2, 0)] != "":
+            return True
+        
+        return False
+
     def on_mouse_press(self, x, y, button, modifiers):
-        x_arena = x // 200
-        y_arena = y // 200
+        self.x_arena = x // 200
+        self.y_arena = y // 200
 
-        if self.arena[(x_arena,y_arena)] == "":
-            self.arena[(x_arena,y_arena)] = self.spieler
-        self.spieler = "O" if self.spieler == "X" else "X"
+        if self.arena[(self.x_arena, self.y_arena)] == "" and not self._gewinnprüfung():
+            self.arena[(self.x_arena, self.y_arena)] = self.spieler
+            self.spieler = "O" if self.spieler == "X" else "X"
+       
 
+    def freie_felder(self):
+        freie_felder_liste = []
+        for i in range(3):
+            for j in range(3):
+                if self.arena[(i, j)]:
+                    freie_felder_liste.append(self.arena[(i, j)])
+                    return True
+                    print("t")
+                          
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.R:
+            self.reset() 
+        if symbol == arcade.key.Q:
+            arcade.exit()
 
-
-    
     def on_update(self, delta_time):
         ...
 
@@ -35,5 +78,12 @@ class TTT(arcade.Window):
             y_fenster = y_arena * 200 +100 + 6
             feld_inhalt = self.arena[koordinaten]
             arcade.draw_text(feld_inhalt, x_fenster, y_fenster, font_size=120, font_name="Garamond", anchor_x="center", anchor_y="center", color = arcade.color.GOLD)
+        if self._gewinnprüfung():
+            if self.spieler == "O":
+                arcade.draw_rectangle_filled(self.width / 2, self.height / 2, self.width, self.height, arcade.make_transparent_color(arcade.color.BLACK, 150))
+                arcade.draw_text("Du hast Gewonnen, Spieler " + ("X" if self.spieler == "O" else "O"), self.width / 2, self.height /2 , color=arcade.color.WHITE , font_size= 90 , font_name="Garamond", width=self.width, align="center" , anchor_x ="center", anchor_y="center", multiline=True)
+        if self.freie_felder == True:
+            arcade.draw_rectangle_filled(self.width / 2, self.height / 2, self.width, self.height, arcade.make_transparent_color(arcade.color.BLACK, 150))
+            arcade.draw_text("Es Ist unentschieden", self.width / 2, self.height /2 , color=arcade.color.WHITE , font_size= 90 , font_name="Garamond", width=self.width, align="center" , anchor_x ="center", anchor_y="center", multiline=True)
 TTT()   
 arcade.run()
